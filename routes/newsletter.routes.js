7abe951c591a1ca96c38f5e33ebcd93ed7274ee4
and_jalu <and.jalu@upi.edu>
2023-12-11 15:58:22 +0700

    edit newsletter

--- routes/newsletter.routes.js
+++ routes/newsletter.routes.js
@@ -4,23 +4,22 @@ const { prisma } = require("../config/prisma");
 
 
 newsletterRoutes.get("/", async (req, res) => {
-	const newsletters = await prisma.newsletter.findMany();
-	res.status(200).send(newsletters);
+    const newsletters = await prisma.newsletter.findMany();
+    res.status(200).send(newsletters);
 });
 
 // create new message
 newsletterRoutes.post("/", async (req, res) => {
-	const { name, email, newsletter } = req.body;
-	const newnewsletter = await prisma.newsletter.create({
-		data: {
-			name: name,
-			email: email,
-		},
-	});
-	res.status(201).json({
-		message: "newsletter created",
-		data: newnewsletter,
-	});
+    const { name, email, newsletter } = req.body;
+    const newnewsletter = await prisma.newsletter.create({
+        data: {
+            email: email,
+        },
+    });
+    res.status(201).json({
+        message: "newsletter created",
+        data: newnewsletter,
+    });
 });
 
 module.exports = { newsletterRoutes };
