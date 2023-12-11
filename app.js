604a21387c85fa558777dfbdce16518b61a5fb84
and_jalu <and.jalu@upi.edu>
2023-12-11 14:00:46 +0700

    update

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 require("dotenv").config()
 const express = require("express");
 const cors = require("cors")
-const { prisma } = require("./config/prisma.js");
+const { feedbackRoutes } = require("./routes/feedback.routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
+
 app.get("/", async (req, res) => {
     res.send("here is the response");
 });
 
-app.get ("/feedback", async (req, res) => {
-    const feedback = await prisma.feedback.findMany()
-    res.status(200).send(feedback);
-})
+app.use("/feedback", feedbackRoutes);
+
 app.all("*", async (req, res) => {
     res.json({
         message: "routes you are looking is not found"
